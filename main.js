33677a65aa74c905e79eb6edff48132bf706154b
Zen <alexander.merab@gmail.com>
2022-11-28 07:49:57 -0600

    Setup array

---
+++ main.js
@@ -0,0 +1,18 @@
+const choices = ["rock","paper","scissors"]
+function game() {
+    //play the game
+    //play five rounds
+    //console based
+}
+
+function playround(){
+    const playerSelection =
+}
+
+function playerChoice() {
+    //get input from player
+}
+
+function computerChoice(){
+    //get random input
+}
